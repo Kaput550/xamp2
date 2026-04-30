7507a6fa4247c23b34030c68776d15bfeebaaad0
Kaput550 <jisaacgm7@gmail.com>
2026-04-30 00:37:03 -0600

    aw

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ sudo docker compose down
 
 #Inicializamos contenedores y reconstruimos
 echo "Inicializamos contenedores y reconstruimos imagen actualizada"
-sudo docker compose -f docker-compose.production.yml up -d --build
+sudo docker compose -f docker-compose-production.yml up -d --build
 
 echo "Contenedores actualizados y en ejecución"
